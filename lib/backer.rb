dfde3773ecb0527b0aa0891373477a1d60e6629e
Amar Chatman <amarjerome@gmail.com>
2018-05-15 23:01:32 +0000

    automatically backed up by learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -11,7 +11,7 @@ class Backer
 
   def back_project(project)
     @backed_projects << project
-    @backed_projects << project.add_backer(self)
+    project.add_backer(self)
   end
 
   def backed_projects
